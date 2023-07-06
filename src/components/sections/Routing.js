25625e761b2a469e3adacfd7c148cc7d6769fa19
websitedsigns <websitedsigns@gmail.com>
2023-07-06 18:52:16 +0100

    App now works
    Need to play about with formatting of wording bunching to much

--- src/components/sections/Routing.js
+++ src/components/sections/Routing.js
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import Homepage from "./pages/HomePage";
-import About from "./pages/About";
-import Reservation from "./pages/BookingPage";
-import Order from "./pages/Order";
-import Login from "./pages/Login";
-import Confirmation from "./pages/Confirmation";
+import Homepage from "../pages/HomePage";
+import About from "../pages/About";
+import Reservation from "../pages/BookingPage";
+import Order from "../pages/Order";
+import Login from "../pages/Login";
+import Confirmation from "../pages/Confirmation";
 
 export default function Routing() {
   return (
