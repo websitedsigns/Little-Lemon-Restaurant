d8004c8dbd849ffdf53ea20fefca2369aa9689d9
websitedsigns <websitedsigns@gmail.com>
2023-09-02 10:51:24 +0100

    Update TestimonialCarousel.js

--- src/components/sections/headingPages/TestimonialCarousel.js
+++ src/components/sections/headingPages/TestimonialCarousel.js
@@ -5,19 +5,19 @@ export default function CarouselPage() {
     return (
         <Carousel infiniteLoop={true} autoPlay={true} Interval={5000} showStatus={false}>
             <TestimonialCard name="Micheal Caldwell" description="This is the best Mediterranean food that I've ever had!"/>
-            <TestimonialCard name="Alan Chen" description="My Shiba Inu, Mugi, really loved the cozy vibes and 
+            <TestimonialCard name="John Smith" description="Really loved the cozy vibes and 
                 delicious food here."/>
-            <TestimonialCard name="Casey Lau" description="I've had some great Mediterranean food before, but none of them beats
+            <TestimonialCard name="Erika Rushton" description="I've had some great Mediterranean food before, but none of them beats
              Little Lemon in flavor and texture."/>
             <TestimonialCard name="John Rosenblum" description="Great food, welcoming staff, cozy atmosphere. A great place to 
                 treat your kids to."/>
-            <TestimonialCard name="Jim Reynor" description="The food here really refreshed me after a late night shift
+            <TestimonialCard name="Jim Rayner" description="The food here really refreshed me after a late night shift
                 at the local supply depot."/>
             <TestimonialCard name="Brian Dean" description="I came to Little Lemon after a 3 hour flight from the med.
                  The food here tasted so delicious was like being back at home."/>
-            <TestimonialCard name="Tyler Tohmine" description="The food here was fire!! Everyone should try this place
+            <TestimonialCard name="Tyler Johnson" description="The food here was fire!! Everyone should try this place
                  out at least once if they live in London or are visiting."/>
-            <TestimonialCard name="Jack Hu" description="This restaurant served as a perfect dinner for me after a 
+            <TestimonialCard name="Jack Watkins" description="This restaurant served as a perfect dinner for me after a 
                 long night of studying. I would definitely order from here again!"/>
              </Carousel>
     )
